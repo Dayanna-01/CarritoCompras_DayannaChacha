ef2a8fbb658861c584b9d135481ba4c97092de8e
Dayanna Chacha <jchachav@est.ups.edu.ec>
2025-06-29 21:10:00 -0500

    Commit Avanzado: Ingreso de cambio de idiomas

--- src/main/java/ec/edu/ups/poo/vista/LoginView.java
+++ src/main/java/ec/edu/ups/poo/vista/LoginView.java
@@ -1,6 +1,9 @@
 package ec.edu.ups.poo.vista;
 
+import ec.edu.ups.poo.util.MensajeInternacionalizacionHandler;
+
 import javax.swing.*;
+import java.util.Locale;
 
 public class LoginView extends JFrame{
     private JPanel panelPrincipal;
@@ -8,13 +11,80 @@ public class LoginView extends JFrame{
     private JPasswordField txtContrasenia;
     private JButton btnIniciar;
     private JButton btnRegistrar;
+    private JButton btnOlvidar;
+    private JButton btnSalir;
+    private JLabel lblTitulo;
+    private JLabel lblUsuario;
+    private JLabel lblContrasenia;
+    private JComboBox cbxIdiomas;
+    private JLabel lblIdioma;
+    private MensajeInternacionalizacionHandler mi;
 
-    public LoginView() {
+    public LoginView(MensajeInternacionalizacionHandler mi) {
+        this.mi = mi;
         setTitle("Iniciar Sesion");
         //setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setSize(600, 400);
         setLocationRelativeTo(null);
+        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         add(panelPrincipal);
+        inicializarComponentes();
+    }
+
+    public JComboBox getCbxIdiomas() {
+        return cbxIdiomas;
+    }
+
+    public void setCbxIdiomas(JComboBox cbxIdiomas) {
+        this.cbxIdiomas = cbxIdiomas;
+    }
+
+    public JLabel getLblIdioma() {
+        return lblIdioma;
+    }
+
+    public void setLblIdioma(JLabel lblIdioma) {
+        this.lblIdioma = lblIdioma;
+    }
+
+    public JLabel getLblTitulo() {
+        return lblTitulo;
+    }
+
+    public void setLblTitulo(JLabel lblTitulo) {
+        this.lblTitulo = lblTitulo;
+    }
+
+    public JLabel getLblUsuario() {
+        return lblUsuario;
+    }
+
+    public void setLblUsuario(JLabel lblUsuario) {
+        this.lblUsuario = lblUsuario;
+    }
+
+    public JLabel getLblContrasenia() {
+        return lblContrasenia;
+    }
+
+    public void setLblContrasenia(JLabel lblContrasenia) {
+        this.lblContrasenia = lblContrasenia;
+    }
+
+    public JButton getBtnSalir() {
+        return btnSalir;
+    }
+
+    public void setBtnSalir(JButton btnSalir) {
+        this.btnSalir = btnSalir;
+    }
+
+    public JButton getBtnOlvidar() {
+        return btnOlvidar;
+    }
+
+    public void setBtnOlvidar(JButton btnOlvidar) {
+        this.btnOlvidar = btnOlvidar;
     }
 
     public JTextField getTxtUsername() {
@@ -58,4 +128,40 @@ public class LoginView extends JFrame{
                 JOptionPane.YES_NO_OPTION, JOptionPane.QUESTION_MESSAGE);
         return respuesta == JOptionPane.YES_OPTION;
     }
+
+    public void inicializarComponentes() {
+        cbxIdiomas.removeAllItems();
+        cbxIdiomas.addItem(mi.get("menu.idioma.es")); // Español
+        cbxIdiomas.addItem(mi.get("menu.idioma.en")); // Inglés
+        cbxIdiomas.addItem(mi.get("menu.idioma.fr")); // Francés
+        actualizarTextos();
+    }
+
+    public void actualizarTextos() {
+        setTitle(mi.get("app.titulo"));
+        lblTitulo.setText(mi.get("login.titulo"));
+        lblUsuario.setText(mi.get("login.usuario"));
+        lblContrasenia.setText(mi.get("login.contrasenia"));
+        lblIdioma.setText(mi.get("login.idioma"));
+
+        btnIniciar.setText(mi.get("login.boton.iniciar"));
+        btnRegistrar.setText(mi.get("login.boton.registrar"));
+        btnOlvidar.setText(mi.get("login.boton.olvidar"));
+        btnSalir.setText(mi.get("login.boton.salir"));
+        Locale currentLocale = mi.getLocale();
+        int selectedIndex = 0;
+        if ("en".equals(currentLocale.getLanguage()) && "US".equals(currentLocale.getCountry())) {
+            selectedIndex = 1;
+        } else if ("fr".equals(currentLocale.getLanguage()) && "FR".equals(currentLocale.getCountry())) {
+            selectedIndex = 2;
+        }
+        if (cbxIdiomas.getSelectedIndex() != selectedIndex) {
+            cbxIdiomas.setSelectedIndex(selectedIndex);
+        }
+        UIManager.put("OptionPane.yesButtonText", mi.get("dialogo.boton.si"));
+        UIManager.put("OptionPane.noButtonText", mi.get("dialogo.boton.no"));
+        UIManager.put("OptionPane.cancelButtonText", mi.get("dialogo.boton.cancelar"));
+        UIManager.put("OptionPane.okButtonText", mi.get("dialogo.boton.aceptar"));
+    }
+
 }
